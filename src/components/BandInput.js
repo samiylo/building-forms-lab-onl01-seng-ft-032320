ba39aea9403e55b4f1b849cc1cb03705716f8377
samiylo <skryshu@gmail.com>
2020-08-07 13:19:24 -0500

    Done.

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -2,10 +2,37 @@
 import React, { Component } from 'react'
 
 class BandInput extends Component {
+  state = {
+    name: ''
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    // const band = this.state.name
+    this.props.addBand(this.state)
+
+    this.setState({
+      name: ''
+    })
+  }
+
   render() {
+
     return(
       <div>
-        Band Input
+        <form onSubmit={this.handleSubmit}>
+          <label>Band</label>
+          <br/>
+          <input type='text' value={this.state.name} onChange={this.handleChange}></input>
+          <button type='submit'>Submit</button>
+        </form>
       </div>
     )
   }
--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,52 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import BandInput from '../components/BandInput'
+import { addBand } from '../components'
 
 class BandsContainer extends Component {
+
+  
   render() {
+
+    const bandsList = this.props.bands.map((band, key) => <li key={key}>{band.name}</li>)
+
     return(
       <div>
-        BandsContainer
+        <BandInput addBand={this.props.addBand}/>
+        <h2>Bands List</h2>
+        {bandsList}
       </div>
     )
   }
 }
 
-export default BandsContainer
+const mapStateToProps = state => {
+  return {
+    bands: state.bands
+  }
+}
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     addBand: (band) => dispatch({type: 'ADD_BAND', band})
+//   }
+// }
+
+
+export default connect(mapStateToProps, {addBand})(BandsContainer)
+
+
+const addBand = band => {
+  return {
+    type: "ADD_BAND",
+    band
+  }
+}
+
+const getBands = () => {
+  const bands = fetch(URL)
+  return {
+    type: 'GET_BANDS',
+    bands: bands
+  }
+}
